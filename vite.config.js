20b4bdb2726f30d14f16e15ae4063b4a441fe428
Jessica Langis <jlynn198@gmail.com>
2026-03-31 09:26:02 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react'
 // NOTE: vite-plugin-pwa will be added in Phase 2 once the core app is stable.
 // PWA manifest config is ready and waiting in this comment block:
 //
-//
 // VitePWA({
 //   registerType: 'autoUpdate',
 //   manifest: {
